aececfed67606c13fe7ab6ec8cde0b22025741a0
Zachary Kuchar <zacharyk323@live.com>
2023-08-24 11:29:54 -0400

    Updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>
-      <nav><h4>Navbar</h4></nav>
-    </div>
+    <nav>
+      <Link to="/orders">Order History</Link>
+      {/* &nbsp No break spacing creates a space that cannot be pushed onto another line by word wrap */}
+      &nbsp; | &nbsp;
+      <Link to="/orders/new">New Order</Link>
+    </nav>
   )
 }
 
