90f2c497f158f5aa3e0f7fc29e594afe846d1d36
Zachary Kuchar <zacharyk323@live.com>
2023-08-28 19:38:16 -0400

    Server side sign in functions

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,5 +1,6 @@
 const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
+const bycrypt = require("bcrypt");
 
 const create = async (req, res) => {
   // Baby step...
@@ -13,7 +14,37 @@ const create = async (req, res) => {
     console.log("token printing");
     res.json(token);
   } catch (error) {
-    console.log(error);
+    // console.log(error);
+    res.status(400).json(error);
+  }
+};
+
+const signIn = async (req, res) => {
+  try {
+    console.log("Body for login", req.body);
+
+    const user = await User.findOne({ email: req.body.email });
+    console.log("Did we find one??", user);
+
+    if (!user) {
+      //Did not find a user
+      console.log("No user found");
+      res.status(400).json(error);
+    }
+    const match = await bycrypt.compare(req.body.password, user.password);
+    console.log("match is ", match);
+
+    if (match) {
+      console.log("Good sign in");
+      const token = createJWT(user);
+      res.json(token);
+    } else {
+      console.log("Bad sign in");
+      return res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
+    }
+
+    // res.json("Bad sign in");
+  } catch (error) {
     res.status(400).json(error);
   }
 };
@@ -27,4 +58,4 @@ function createJWT(user) {
   );
 }
 
-module.exports = { create };
+module.exports = { create, signIn };
