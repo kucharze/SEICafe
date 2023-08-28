e3eb86e737fe101a34888e43b497c13c24697307
Zachary Kuchar <zacharyk323@live.com>
2023-08-28 12:01:22 -0400

    Initial logic to post to the database

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,16 @@
-const create = (req, res) => {
+const userModel = require("../../models/User");
+const jwt = require("jsonwebtoken");
+
+const create = async (req, res) => {
   // Baby step...
   console.log("Body: ", req.body);
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email,
-    },
-  });
+  // res.json(req.body);
+
+  try {
+    const user = await User.create(req.body);
+  } catch (error) {
+    res.status(400).jdon(err);
+  }
 };
 
 module.exports = { create };
