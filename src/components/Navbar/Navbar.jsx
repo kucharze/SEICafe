d7a92637ec7ce060bc89e4afb0ec8c67008cccb7
Zachary Kuchar <zacharyk323@live.com>
2023-08-28 15:31:10 -0400

    Add user and logout button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// Using the import below, we can call any exported function using: userService.someMethod()
+import * as userService from '../../utilities/users-service';
 
-function Navbar() {
+function Navbar({user,setUser}) {
+
+  const logout = () =>{
+    userService.logOut()
+
+    setUser(null)
+  }
   return (
-    <nav>
+    <nav className='nav'>
       <Link to="/orders">Order History</Link>
       {/* &nbsp No break spacing creates a space that cannot be pushed onto another line by word wrap */}
       &nbsp; | &nbsp;
       <Link to="/orders/new">New Order</Link>
+      &nbsp; | &nbsp;
+      {
+        user && <span>Welcome {user}</span>
+      }
+      &nbsp;&nbsp;<Link to="" onClick={logout}>Log Out</Link>
     </nav>
   )
 }
