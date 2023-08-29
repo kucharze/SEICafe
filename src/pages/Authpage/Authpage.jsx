18c308fae74b0870a1ed0d442bfb95cca5fed194
Zachary Kuchar <zacharyk323@live.com>
2023-08-29 09:53:07 -0400

    Add button

--- src/pages/Authpage/Authpage.jsx
+++ src/pages/Authpage/Authpage.jsx
@@ -1,13 +1,21 @@
 import LoginForm from "../../components/Loginform/Loginform"
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
+import React, {useState} from 'react'
 
 function Authpage({setUser}) {
+  const [showLogin,setShowLogin] = useState(true)
+
   return (
-    <div>
-      <h1>Auth Page Login</h1>
-      <SignUpForm setUser={setUser}/>
-      <LoginForm setUser={setUser}/>
-    </div>
+    <main>
+      <div>
+        <button onClick={()=>{setShowLogin(!showLogin)}}>{showLogin? 'Sign up' : 'Login'}</button>
+        {
+          showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser}/>
+          
+        }
+        
+      </div>
+    </main>
   )
 }
 
