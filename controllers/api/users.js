475145de5a82792b669e38f77715d8efcc3b15c6
Zachary Kuchar <zacharyk323@live.com>
2023-08-29 15:29:01 -0400

    Return token data and check if user is logged in

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -54,4 +54,11 @@ function createJWT(user) {
   );
 }
 
-module.exports = { create, signIn };
+function checkToken(req, res) {
+  // req.user will always be there for you when a token is sent
+  console.log("req.user", req.user);
+  console.log(req.exp);
+  res.json(req.exp);
+}
+
+module.exports = { create, signIn, checkToken };
