5139a9cfaf246dbcd1fc7db701b3e7f147b1edd7
Zachary Kuchar <zacharyk323@live.com>
2023-08-30 15:03:58 -0400

    New utilities

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,29 +1,30 @@
 import * as usersAPI from "./users-api";
 
 export async function signUp(userData) {
-  //Errors will propgate up to the handlesubmit
-  // Delegate the network request code to the users-api.js API module
-  // which will ultimately return a JSON Web Token (JWT)
+  // Delete the network request code to the
+  // users-api.js module which will ultimately
+  // return the JWT
   const token = await usersAPI.signUp(userData);
-
-  //Persist the token in local storage
+  // Persist the token to localStorage
   localStorage.setItem("token", token);
-
-  // return token;
   return getUser();
 }
 
-//Grab token from local storage
+export async function login(credentials) {
+  const token = await usersAPI.login(credentials);
+  // Persist the token to localStorage
+  localStorage.setItem("token", token);
+  return getUser();
+}
+
 export function getToken() {
-  // getItem returns null if there's no string
   const token = localStorage.getItem("token");
+  // getItem will return null if no key
   if (!token) return null;
-  // Obtain the payload of the token
   const payload = JSON.parse(atob(token.split(".")[1]));
-  //console.log(payload);
-  // A JWT's exp is expressed in seconds, not milliseconds, so convert
+  // A JWT's expiration is expressed in seconds, not miliseconds
   if (payload.exp < Date.now() / 1000) {
-    // Token has expired - remove it from localStorage
+    // Token has expired
     localStorage.removeItem("token");
     return null;
   }
@@ -32,24 +33,9 @@ export function getToken() {
 
 export function getUser() {
   const token = getToken();
-  // If there's a token, return the user in the payload, otherwise return null
   return token ? JSON.parse(atob(token.split(".")[1])).user : null;
 }
 
 export function logOut() {
   localStorage.removeItem("token");
-}
-
-export async function login(credentials) {
-  let token = await usersAPI.login(credentials);
-
-  localStorage.setItem("token", token);
-
-  return getUser();
-  //return token
-}
-
-export async function checkToken() {
-  // alert("Clicked in checkedtoken");
-  return usersAPI.checkToken().then((dateStr) => new Date(dateStr));
 }
