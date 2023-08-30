bc81300118124de5cdc4219d15517decc9c135cb
Zachary Kuchar <zacharyk323@live.com>
2023-08-30 13:17:48 -0400

    Added atch all navigation route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { getUser } from './utilities/users-service';
 import "./App.css";
 
@@ -21,6 +21,7 @@ function App() {
         <Routes>
           <Route path="/orders/new" element={<NewOrderPage />} />
           <Route path="/orders" element={<OrderHistoryPage />} />
+          <Route path="/*" element={<Navigate to="/orders/new" />} />
         </Routes>
         </>
       ) : (
