4f7fe6a3deb2ca63dd807a715cdf738f1d915d99
Zachary Kuchar <zacharyk323@live.com>
2023-08-30 14:13:09 -0400

    Auth page style changes

--- src/pages/Authpage/Authpage.jsx
+++ src/pages/Authpage/Authpage.jsx
@@ -1,16 +1,18 @@
 import LoginForm from "../../components/Loginform/Loginform"
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 import React, {useState} from 'react'
+import styles from './AuthPage.module.css'
 
 function Authpage({setUser}) {
   const [showLogin,setShowLogin] = useState(true)
 
   return (
-    <main>
+    <main className={styles.AuthPage}>
       <div>
-        <button onClick={()=>{setShowLogin(!showLogin)}}>
+        <h1>Auth page</h1>
+        <h3 onClick={()=>{setShowLogin(!showLogin)}}>
           {showLogin? 'Sign up' : 'Login'}
-          </button>
+          </h3>
         {
           showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser}/>
           
