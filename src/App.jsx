0e798b1e7fcbd2e6cc44fe7e1809ee61b422aca1
Zachary Kuchar <zacharyk323@live.com>
2023-08-30 15:11:16 -0400

    Fixed missing props

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
         
         <Navbar user={user.name} setUser={setUser} />
         <Routes>
-          <Route path="/orders/new" element={<NewOrderPage user={user} />} />
-          <Route path="/orders" element={<OrderHistoryPage user={user} />} />
+          <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
+          <Route path="/orders" element={<OrderHistoryPage user={user} setUser={setUser} />} />
           <Route path="/*" element={<Navigate to="/orders/new" />} />
         </Routes>
         </>
